b358efcebcf927e9d3d731ca872e582664145b2a
bobby <ande970s@stud.kea.dk>
2022-10-14 10:11:27 +0200

    Cannot resolve name fejl

--- src/main/java/com/example/thymleaf/Controller/ArmyController.java
+++ src/main/java/com/example/thymleaf/Controller/ArmyController.java
@@ -5,10 +5,7 @@ import com.example.thymleaf.Repository.ArmyRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class ArmyController {
